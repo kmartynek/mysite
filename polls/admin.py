121f8f7e2fe9591087c805c893a6f6d7dc4eb9d3
kmartynek <kmartynek@gmail.com>
2020-06-23 06:44:23 +0200

    update

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['question_text', 'pub_date']
+    search_fields = ['question_text']
+    ordering = ['-pub_date']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text','question', 'votes']
+    search_fields = ['choice_text']
+    list_filter = ['choice_text', 'question', 'votes']
+    autocomplete_fields = ['question']
+    ordering = ['question','-votes']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.datastructures import  MultiValueDictKeyError
+from .models import Question,Choice
 
-# Create your views here.
+from django.views.decorators.http import require_POST
+
+
+def index(request):
+    questions = Question.objects.all()
+    title = 'Lista wszystkich pytań'
+    context = {
+        'questions': questions,
+        'title' : title,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    #question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, id=question_id)
+    title = f'Pytanie: {question.question_text}'
+
+    context = {
+        'question': question,
+        'title': title,
+    }
+    return render(request, 'polls/detail.html', context)
+
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+
+    if request.method == "POST":
+        try:
+            choice_form = request.POST['choice']
+        except MultiValueDictKeyError:
+            return redirect('polls:detail', question_id)
+
+        try:
+            selected_choice = question.choice_set.get(id=choice_form)
+        except Choice.DoesNotExist:
+            return redirect('polls:detail', question_id)
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return redirect('polls:results', question_id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    title = f'Wyniki: {question.question_text}'
+
+    context = {
+        'question' : question,
+        'title' : title,
+    }
+
+    return render(request, 'polls/results.html', context)
+
+
+
+
